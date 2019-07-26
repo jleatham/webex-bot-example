5dc706e5fceb695da54d25f38544d83cc1b1db9c
jleatham <j.leatham@gmail.com>
2019-07-26 09:21:01 -0500

    TShoot

--- botFunctions.py
+++ botFunctions.py
@@ -108,7 +108,12 @@ def process_bot_input_command(room_id,command, headers, bot_name):
             process_stock_query(room_id,stock_query,headers)
 
     else:
-        bot_post_to_room(room_id,"Only commands I know are: **TEST** , and **pause** .  All values hard-coded at the moment and messages sent on schedule.",headers)
+        msg_list.append("How to use bot: \n\n")
+        msg_list.append("**example** <city>  --> don't include <city> if you want a random selection \n\n")
+        msg_list.append("If you are in a room with multiple people, be sure to @ the bot \n\n")
+        msg_list.append("@KDRP-stock-bot **example** dallas \n\n")
+        msg = ''.join(msg_list)        
+        bot_post_to_room(room_id,"",headers)
 
 
 
