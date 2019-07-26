31ed093d8d34757c3ffcfc759005035132013a56
jleatham <j.leatham@gmail.com>
2019-07-26 09:28:23 -0500

    TShoot

--- botFunctions.py
+++ botFunctions.py
@@ -108,12 +108,13 @@ def process_bot_input_command(room_id,command, headers, bot_name):
             process_stock_query(room_id,stock_query,headers)
 
     else:
+        msg_list = []
         msg_list.append("How to use bot: \n\n")
         msg_list.append("**example** <city>  --> don't include <city> if you want a random selection \n\n")
         msg_list.append("If you are in a room with multiple people, be sure to @ the bot \n\n")
         msg_list.append("@KDRP-stock-bot **example** dallas \n\n")
         msg = ''.join(msg_list)        
-        bot_post_to_room(room_id,"",headers)
+        bot_post_to_room(room_id,msg,headers)
 
 
 
